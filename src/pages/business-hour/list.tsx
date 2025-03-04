667d3e17e667b9a55528045fc3207f7243ff3b5f
Hadi Torabi <83789371+haditorabi@users.noreply.github.com>
2025-03-04 10:52:09 -0500

    Updated

---
+++ src/pages/business-hour/list.tsx
@@ -0,0 +1,179 @@
+import { AMENITY_DELETE_QUERY, AMENITY_LIST_QUERY } from "./queries";
+import React from "react";
+import { useTable } from "@refinedev/react-table";
+import { type ColumnDef, flexRender } from "@tanstack/react-table";
+import { type GetManyResponse, useMany } from "@refinedev/core";
+import {
+  List,
+  ShowButton,
+  EditButton,
+  DeleteButton,
+  DateField,
+} from "@refinedev/chakra-ui";
+
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  HStack,
+  Box,
+  Select,
+} from "@chakra-ui/react";
+
+import { ColumnFilter, ColumnSorter } from "../../components/table";
+import { Pagination } from "../../components/pagination";
+
+export const BusinessHourList: React.FC = () => {
+  const columns = React.useMemo<ColumnDef<any>[]>(
+    () => [
+      {
+        id: "id",
+        header: "ID",
+        accessorKey: "id",
+        enableColumnFilter: false,
+      },
+      {
+        id: "name",
+        header: "Name",
+        accessorKey: "name",
+        meta: {
+          filterOperator: "contains",
+        },
+      },
+      {
+        id: "createdAt",
+        header: "Created At",
+        accessorKey: "createdAt",
+        cell: function render({ getValue }) {
+          return <DateField value={getValue() as string} format="LLL" />;
+        },
+        enableColumnFilter: false,
+      },
+      {
+        id: "actions",
+        header: "Actions",
+        accessorKey: "id",
+        enableColumnFilter: false,
+        enableSorting: false,
+        cell: function render({ getValue }) {
+          return (
+            <HStack>
+              <ShowButton
+                resource="amenity"
+                hideText
+                size="sm"
+                recordItemId={getValue() as string}
+              />
+              <EditButton
+                hideText
+                size="sm"
+                recordItemId={getValue() as string}
+              />
+              <DeleteButton
+                hideText
+                size="sm"
+                recordItemId={getValue() as string}
+                meta={{ gqlMutation: AMENITY_DELETE_QUERY }}
+              />
+            </HStack>
+          );
+        },
+      },
+    ],
+    []
+  );
+
+  const {
+    getHeaderGroups,
+    getRowModel,
+    setOptions,
+    refineCore: {
+      setCurrent,
+      pageCount,
+      current,
+      tableQuery: { data: tableData },
+    },
+  } = useTable({
+    columns,
+    refineCoreProps: {
+      initialSorter: [
+        {
+          field: "id",
+          order: "desc",
+        },
+      ],
+      meta: {
+        gqlQuery: AMENITY_LIST_QUERY,
+      },
+    },
+  });
+
+  setOptions((prev) => ({
+    ...prev,
+    meta: {
+      ...prev.meta,
+    },
+  }));
+
+  return (
+    <List title="Amenity">
+      <TableContainer>
+        <Table variant="simple" whiteSpace="pre-line">
+          <Thead>
+            {getHeaderGroups().map((headerGroup) => (
+              <Tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <Th key={header.id}>
+                      {!header.isPlaceholder && (
+                        <HStack spacing="xs">
+                          <Box>
+                            {flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                          </Box>
+                          <HStack spacing="xs">
+                            <ColumnSorter column={header.column} />
+                            <ColumnFilter column={header.column} />
+                          </HStack>
+                        </HStack>
+                      )}
+                    </Th>
+                  );
+                })}
+              </Tr>
+            ))}
+          </Thead>
+          <Tbody>
+            {getRowModel().rows.map((row) => {
+              return (
+                <Tr key={row.id}>
+                  {row.getVisibleCells().map((cell) => {
+                    return (
+                      <Td key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </Td>
+                    );
+                  })}
+                </Tr>
+              );
+            })}
+          </Tbody>
+        </Table>
+      </TableContainer>
+      <Pagination
+        current={current}
+        pageCount={pageCount}
+        setCurrent={setCurrent}
+      />
+    </List>
+  );
+};
---
+++ src/pages/user-verification/queries.ts
@@ -0,0 +1,55 @@
+import gql from "graphql-tag";
+
+export const AMENITY_LIST_QUERY = gql`
+  query Amenity($limit: Int, $page: Int) {
+    allAmenity(limit: $limit, page: $page) {
+      items {
+        createdAt
+        id
+        mediaId
+        name
+      }
+      totalCount
+    }
+  }
+`;
+export const AMENITY_SHOW_QUERY = gql`
+  query Amenity($id: String!) {
+    amenity(id: $id) {
+      id
+      name
+      mediaId
+      createdAt
+    }
+  }
+`;
+export const AMENITY_DELETE_QUERY = gql`
+  mutation Amenity($id: String!) {
+    deleteAmenity(id: $id) {
+      id
+      name
+      mediaId
+      createdAt
+    }
+  }
+`;
+export const AMENITY_EDIT_MUTATION = gql`
+  mutation Amenity($id: String!, $data: UpdateAmenityInput!) {
+    updateAmenity(id: $id, data: $data) {
+      id
+      name
+      mediaId
+      createdAt
+    }
+  }
+`;
+export const AMENITY_CREATE_MUTATION = gql`
+  mutation Amenity($data: CreateAmenityInput!) {
+    createAmenity(data: $data) {
+      id
+      name
+      mediaId
+      createdAt
+    }
+  }
+`;
